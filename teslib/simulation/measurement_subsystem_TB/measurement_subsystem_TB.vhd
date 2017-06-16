34cee58681077d17d9f70bef62542c6be090939b
Geoff Gillett <gillett@physics.uq.edu.au>
2017-06-17 05:31:21 +1000

    sim changes

--- teslib/simulation/measurement_subsystem_TB/measurement_subsystem_TB.vhd
+++ teslib/simulation/measurement_subsystem_TB/measurement_subsystem_TB.vhd
@@ -248,7 +248,7 @@ slope_config(1).reload_data <= (others => '0');
 slope_config(1).reload_last <= '0';
 slope_config(1).reload_valid <= '0';
 
-chan_reg(0).baseline.offset <= to_signed(-3230*8,DSP_BITS);
+chan_reg(0).baseline.offset <= to_signed(0,DSP_BITS);
 chan_reg(0).baseline.count_threshold <= to_unsigned(30,BASELINE_COUNTER_BITS);
 chan_reg(0).baseline.threshold <= (others => '1'); --to_unsigned(700,BASELINE_BITS-1);--(others => '1');
 chan_reg(0).baseline.new_only <= TRUE;
